d4018325bbb1d152879b12d5bec877e4195fbd5a
TrayMachi <tristan.agra@ui.ac.id>
2025-04-28 18:51:52 +0700

    feat: course module

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
+import { AccessTokenGuard } from 'src/auth/auth.guard';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule, AccessTokenGuard],
   controllers: [CourseController],
   providers: [CourseService],
 })
