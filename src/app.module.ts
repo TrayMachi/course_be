02c3a597062c5325a6e1762dbfa79600054d8a42
TrayMachi <tristan.agra@ui.ac.id>
2025-04-28 13:20:53 +0700

    feat: teacher skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { ResponseUtil } from './common/utils/response.util';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { CourseModule } from './course/course.module';
+import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, CourseModule, StudentModule, TeacherModule],
   controllers: [AppController],
   providers: [AppService, ResponseUtil],
 })
