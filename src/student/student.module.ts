00b1e3df642e33c7440a9672d9ea4ba4e070c9e5
TrayMachi <tristan.agra@ui.ac.id>
2025-04-28 16:57:24 +0700

    feat: student module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AccessTokenGuard } from 'src/auth/auth.guard';
 
 @Module({
+  imports: [PrismaModule, AccessTokenGuard],
   controllers: [StudentController],
   providers: [StudentService],
 })
