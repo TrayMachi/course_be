8e46cfeced9979f858ba1da0877a284612de6b40
TrayMachi <tristan.agra@ui.ac.id>
2025-04-28 16:57:14 +0700

    feat: student dto

---
+++ src/student/student.dto.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+import { createZodDto } from 'nestjs-zod';
+
+export const RegisterStudentSchema = z.object({
+  userId: z.string(),
+  GPA: z
+    .number()
+    .min(0, 'GPA must be between 0 and 4')
+    .max(4, 'GPA must be between 0 and 4'),
+  major: z.string().nonempty('Major is required'),
+  degree: z.string().nonempty('Degree is required'),
+});
+
+export class RegisterStudentDto extends createZodDto(RegisterStudentSchema) {}
+
+export const UpdateStudentSchema = z.object({
+  GPA: z
+    .number()
+    .min(0, 'GPA must be between 0 and 4')
+    .max(4, 'GPA must be between 0 and 4'),
+  major: z.string().nonempty('Major is required'),
+  degree: z.string().nonempty('Degree is required'),
+});
+
+export class UpdateStudentDto extends createZodDto(UpdateStudentSchema) {}
