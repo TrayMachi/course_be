953d666853fd4a6b4cca2d0524627cae27af917b
TrayMachi <tristan.agra@ui.ac.id>
2025-04-28 15:48:27 +0700

    feat: init zod globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,25 @@ import { AuthModule } from './auth/auth.module';
 import { CourseModule } from './course/course.module';
 import { StudentModule } from './student/student.module';
 import { TeacherModule } from './teacher/teacher.module';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [PrismaModule, AuthModule, CourseModule, StudentModule, TeacherModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    CourseModule,
+    StudentModule,
+    TeacherModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, ResponseUtil],
+  providers: [
+    AppService,
+    ResponseUtil,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
