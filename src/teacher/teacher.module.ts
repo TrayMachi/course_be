c899fdd46a9318875ac90f7e57b4cee002187f77
TrayMachi <tristan.agra@ui.ac.id>
2025-04-28 18:21:36 +0700

    feat: teacher module

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TeacherService } from './teacher.service';
 import { TeacherController } from './teacher.controller';
+import { AccessTokenGuard } from 'src/auth/auth.guard';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule, AccessTokenGuard],
   controllers: [TeacherController],
   providers: [TeacherService],
 })
